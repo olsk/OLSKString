67586cbe4e8f678080b6d92add4c4f7984b727e9
Rosano Coutinho <pub@xeari.com>
2018-04-16 11:34:51 -0400

    moving files to new prefix

---
+++ main.js
@@ -0,0 +1,46 @@
+/*!
+ * Copyright(c) 2018 Rosano Coutinho
+ * MIT Licensed
+ */
+
+//_ ROCOStringWithFormat
+
+exports.ROCOStringWithFormat = function (inputData) {
+	if (typeof inputData !== 'string') {
+		throw new Error('ROCOErrorInputInvalid');
+	}
+
+	var substitutions = Object.values(arguments).slice(1);
+
+	if (!substitutions.length) {
+		return inputData;
+	}
+
+	var formattedString = inputData;
+
+	(inputData.match(/\%\@/g) || []).forEach(function (e, i) {
+		formattedString = formattedString.replace(e, substitutions[i]);
+	});
+
+	exports._ROCOStringAllMatchesWithRegexAndString(/\%\$(\d*)\@/g, inputData).forEach(function (e) {
+		formattedString = formattedString.replace(e[0], substitutions[e[1] - 1]);
+	});
+
+	return formattedString;
+};
+
+//_ _ROCOStringAllMatchesWithRegexAndString
+
+exports._ROCOStringAllMatchesWithRegexAndString = function (regex, string) {
+	var matches = [];
+
+	var match = regex.exec(string);
+	
+	while (match != null) {
+		matches.push(match);
+
+	  match = regex.exec(string);
+	};
+
+	return matches;
+};
